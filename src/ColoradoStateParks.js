9f98a15adb10a9d8d709b01184edb343af615f62
pwangy <blipsandclicks@gmail.com>
2024-02-19 09:37:04 -0800

    complete lab

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,7 +1,18 @@
 import howManyParks from './parks/howManyParks'
+import MesaVerde from './parks/MesaVerde'
+import { trees as parkTrees } from './parks/RockyMountain'
+import { wildlife, elevation } from './parks/RockyMountain'
 
-const ColoradoStateParks = () => {
+export default function ColoradoStateParks() {
 	howManyParks() // => "42 parks!"
+  console.log(parkTrees)
+  wildlife()
+  elevation()
 
-	return <h1>Colorado State Parks!</h1>
+	return (
+    <>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde /> 
+    </>
+  )
 }
